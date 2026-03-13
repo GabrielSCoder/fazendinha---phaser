db7cd6ea96d1a864bea16f30d8b8061e9e20faa0
gabriel <gabrielsenasaraiva7@gmail.com>
2026-03-12 22:06:01 -0300

    refatoring planting

--- src/controllers/PlantaController.js
+++ src/controllers/PlantaController.js
@@ -62,10 +62,12 @@ export default class PlantaController {
     }
 
     plantSeed(solo) {
+
         if (!this.scene.gameVariables.selectedSeed || !solo || solo.nome != "solo_preparado") return;
         const semente = this.scene.gameVariables.selectedSeed;
         const tipo_plantacao = semente.tipo_plantacao;
 
+        const sprite = solo;
 
         if (!tipo_plantacao) return;
 
@@ -73,24 +75,9 @@ export default class PlantaController {
 
         if (!itemData) return;
 
-        const sprite_del = solo;
-        sprite_del.destroy();
-        this.scene.gameVariables.sprites = this.scene.gameVariables.sprites.filter(s => s && s !== sprite_del && !s.destroyed);
-
-        const scale = itemData.escala || 1;
-        const originX = itemData.origem?.[0] ?? 0.5;
-        const originY = itemData.origem?.[1] ?? 0.5;
         const tipo = "solo_plantado_simples";
 
-        const { w, h } = this.gridUtils.getSpriteFootprint(solo);
-
-        const iso = this.gridUtils.screenToIso(solo.x, solo.y);
-        const startX = Math.round(iso.x - (w / 2 - 0.5));
-        const startY = Math.round(iso.y - (h / 2 - 0.5));
-
-        const screenPos = this.gridUtils.isoToScreen(iso.x, iso.y);
-
-        const sprite = this.scene.spriteUtils.addGameSprite(itemData, screenPos.x, screenPos.y, scale, originX, originY);
+        sprite.setTexture(itemData.img)
 
         const stages = [
             { percent: 1, texture: semente.img_pronta }
@@ -98,11 +85,7 @@ export default class PlantaController {
 
         this.scene.growthController.startGrowth(sprite, semente.tempoColheita * 60 * 1000, stages);
 
-        sprite.footprint = itemData.area || [w, h];
         sprite.tipo = tipo;
-        sprite.gridX = Math.round(startX + w * 0.5 - 0.5);
-        sprite.gridY = Math.round(startY + h * 0.5 - 0.5);
-        sprite.lastFreePos = { startX, startY };
         sprite.isMoving = false;
         sprite.nome = semente.nome;
         sprite.plantado = true;
@@ -113,12 +96,11 @@ export default class PlantaController {
         sprite.harvestTime = solo.harvestTime;
         sprite.xp = semente.xp;
 
-        if (!this.scene.gameVariables.sprites) this.scene.gameVariables.sprites = [];
-        this.scene.gameVariables.sprites.push(sprite);
-        this.scene.cameraController.ignoreInUICamera([...this.scene.gameVariables.sprites]);
-
-        this.gridUtils.markOccupied(sprite, startX, startY, w, h);
         this.gridUtils.recalculateDepthAround(sprite);
+
+        sprite.setAlpha(1)
+        sprite.isQueued = false;
+        sprite.setInteractive({ useHandCursor: true });
 
         this.uiEvents.emit("action:reward", {
             xp: 1,
